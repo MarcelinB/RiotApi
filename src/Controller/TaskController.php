aed31e5838f921611bb8c719ef67c4948a65f309
Marcelin Bertrand <marcelinbertrandmontembault@gmail.com>
2021-12-02 14:02:49 +0100

    Affichage images tout les champions

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -17,11 +17,18 @@ class TaskController extends AbstractController
         $champions = new ChampionManager;
         $dChampions = $champions->getPacket();
         $listFreeChamp = $dChampions->freeChampionIds;
-        $json = file_get_contents('http://ddragon.leagueoflegends.com/cdn/11.23.1/data/en_US/champion/Azir.json' );
-        $pets = json_decode($json, true);
-        //dd($pets);
-        $img = $pets['data']['Azir']['image']['full'];
-        echo '<img src=http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/'. $img .' class="img-rounded">';
+        $json = file_get_contents('http://ddragon.leagueoflegends.com/cdn/11.23.1/data/en_US/champion.json' );
+        $champ = json_decode($json, true);
+        $nameChamp = $champ['data'];
+
+        //dd($nameChamp);
+       foreach($nameChamp as $name)
+           {
+               $img = $name['image']['full'];
+               echo '<img src=http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/'. $img .' class="img-rounded">';
+           }
+
+        //echo '<img src=http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/'. $img .' class="img-rounded">';
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
